c499149555be839e59c07aad03610a9a14e01e33
sandrocalmanti <sandro@casaccia.enea.it>
2020-03-10 04:05:40 +0100

    Add map by province

---
+++ f_df_map.r
@@ -0,0 +1,22 @@
+
+########################################################################
+########################################################################
+
+df_map = function (df,df_bor,df_scale=1) {
+  
+  df <- df %>% filter(totale_casi>0) %>% filter(lat>0)
+  df_ymd <- ymd(as.Date(unique(df$data)))
+  p <- ggplot() +
+    geom_polygon(data = df_bor, aes(x=long, y = lat, group = group), fill="grey", alpha=0.3) +
+    geom_circle( data=df,aes(x0=long, y0=lat, r=(totale_casi/df_scale)), fill='red',alpha=0.5) +
+    ggtitle(paste0("COVID-19  Totale Casi - Giorno: ",df_ymd)) +
+    labs(size="Totale casi") +
+    theme_void() + 
+    theme(legend.position = "none") +
+    coord_fixed() 
+  
+  ggsave(file=paste0('./PLOT/COVID-19_map_',df_ymd,'.png'),plot=p,height=7,width=7)
+  
+  return(p)
+}
+
---
+++ map_province.R
@@ -0,0 +1,26 @@
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(maps)
+library(purrr)
+library(ggforce)
+source('f_df_map.r')
+
+IT <- map_data("world") %>% filter(region=="Italy")
+
+firstday <- ymd("2020-02-24")
+cal <- as.Date(firstday - 1 + seq(firstday:(today() - 1)))
+
+fileURL <- paste0("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province-",
+                  paste0(year(cal),sprintf("%02d",month(cal)),sprintf("%02d",day(cal))),
+                  ".csv")
+
+df <- map(fileURL,read_csv) 
+df_scale <- max(unlist(map(df,pull,'totale_casi')))
+
+p <- map(df,df_map,df_bor=IT,df_scale)
+
+unlink("./PLOTcovid19.mpeg")
+system("convert -delay 10 './PLOT/COVID-19_map_*.png' ./PLOT/covid19.mpeg")
+
+
