5eb6fd8297ead943d5c670cc186bc3b25a19ab96
Sandro <53210493+sandrocalmanti@users.noreply.github.com>
2020-03-14 12:17:48 +0100

    UPGRADE - Recompute scale

--- map_province.R
+++ map_province.R
@@ -17,12 +17,12 @@ fileURL <- paste0("https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dat
                   paste0(year(cal),sprintf("%02d",month(cal)),sprintf("%02d",day(cal))),
                   ".csv")
 
-df <- map(fileURL,read_csv) 
-df_scale <- max(unlist(map(df,pull,'totale_casi')))
+df <- map(fileURL,read.csv) 
+df_scale <- 1000.*max(unlist(map(df,pull,'totale_casi')),na.rm=TRUE)
 
 p <- map(df,df_map,df_bor=IT,df_scale)
 
-unlink("./PLOTcovid19.mpeg")
-system("convert -delay 10 './PLOT/COVID-19_map_*.png' ./PLOT/covid19.mpeg")
+#unlink("./PLOTcovid19.mpeg")
+#system("convert -delay 10 './PLOT/COVID-19_map_*.png' ./PLOT/covid19.mpeg")
 
 
